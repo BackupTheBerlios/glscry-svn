0b0a36395e9914ac3e8652122fa292813c4051ec
aegis <aegis@a68c7e1a-86db-0310-a1c4-e68a6bf46cff>
2004-06-09 00:35:35 +0000

    Adding some other tests, like VBOs and compiled vertex arrays.

--- trunk/triagara/src/Main.cpp
+++ trunk/triagara/src/Main.cpp
@@ -6,6 +6,8 @@
 #include "glew.h"
 #include "Timer.h"
 
+const int triangleCount = 65536;
+const float runFor = 5.0f;
 
 void runTest() {
     glMatrixMode(GL_PROJECTION);
@@ -14,7 +16,6 @@ void runTest() {
 
     Timer timer;
     Uint64 triangles = 0;
-    const float runFor = 5.0f;
 
     glBegin(GL_TRIANGLES);
     while (timer.elapsed() < runFor) {
@@ -28,7 +29,28 @@ void runTest() {
 
     std::cout << "Immediate: " << Uint64(triangles / runFor) << " tri/s" << std::endl;
 
-    const int triangleCount = 1024;
+    GLuint list = glGenLists(1);
+    glNewList(list, GL_COMPILE);
+    glBegin(GL_TRIANGLES);
+    for (int i = 0; i < triangleCount; ++i) {
+        glVertex2f(0, 0);
+        glVertex2f(0, 0);
+        glVertex2f(0, 0);
+    }
+    glEnd();
+    glEndList();
+    glDeleteLists(list, 1);
+
+    timer.step();
+    triangles = 0;
+    while (timer.elapsed() < runFor) {
+        glCallList(list);
+        triangles += triangleCount;
+    }
+    glFinish();
+
+    std::cout << "Display List: " << Uint64(triangles / runFor) << " tri/s" << std::endl;
+
     std::vector<float> vertexArray(3 * triangleCount);
     glEnableClientState(GL_VERTEX_ARRAY);
     glVertexPointer(3, GL_FLOAT, 0, &vertexArray[0]);
@@ -44,6 +66,59 @@ void runTest() {
     glDisableClientState(GL_VERTEX_ARRAY);
 
     std::cout << "Vertex Arrays: " << Uint64(triangles / runFor) << " tri/s" << std::endl;
+
+    if (GLEW_EXT_compiled_vertex_array) {
+        glEnableClientState(GL_VERTEX_ARRAY);
+        glVertexPointer(3, GL_FLOAT, 0, &vertexArray[0]);
+        glLockArraysEXT(0, triangleCount);
+
+        timer.step();
+        triangles = 0;
+        while (timer.elapsed() < runFor) {
+            glDrawArrays(GL_TRIANGLES, 0, triangleCount);
+            triangles += triangleCount;
+        }
+        glFinish();
+
+        glUnlockArraysEXT();
+        glDisableClientState(GL_VERTEX_ARRAY);
+
+        std::cout << "Compiled Vertex Arrays: " << Uint64(triangles / runFor) << " tri/s" << std::endl;
+    } else {
+        std::cout << "Compiled Vertex Array extension not found." << std::endl;
+    }
+
+    if (GLEW_ARB_vertex_buffer_object) {
+        GLuint buffer;
+        glGenBuffersARB(1, &buffer);
+        glBindBufferARB(GL_ARRAY_BUFFER_ARB, buffer);
+
+        glBufferDataARB(
+            GL_ARRAY_BUFFER_ARB,
+            vertexArray.size() * sizeof(vertexArray[0]),
+            &vertexArray[0],
+            GL_STATIC_DRAW_ARB);
+
+        glEnableClientState(GL_VERTEX_ARRAY);
+        glBindBufferARB(GL_ARRAY_BUFFER_ARB, buffer);
+        glVertexPointer(3, GL_FLOAT, 0, NULL);
+
+        timer.step();
+        triangles = 0;
+        while (timer.elapsed() < runFor) {
+            glDrawArrays(GL_TRIANGLES, 0, triangleCount);
+            triangles += triangleCount;
+        }
+        glFinish();
+
+        glDisableClientState(GL_VERTEX_ARRAY);
+        glBindBufferARB(GL_ARRAY_BUFFER_ARB, 0);
+
+        glDeleteBuffersARB(1, &buffer);
+        std::cout << "Vertex Buffer Objects: " << Uint64(triangles / runFor) << " tri/s" << std::endl;
+    } else {
+        std::cout << "Vertex Buffer Object extension not found." << std::endl;
+    }
 
     timer.step();
     SDL_GL_SwapBuffers();
